548423d4e8ff692524097c44bd62dd2e0fc0462e
Douglas S. Green <dsg@gleim.com>
2018-03-05 07:58:37 -0500

    Bug fixes

--- src/PhpToDoChecker.php
+++ src/PhpToDoChecker.php
@@ -9,7 +9,7 @@ class PhpToDoChecker extends PhpFileChecker
     {
         $comments = $this->parser->getComments();
         foreach ($comments as $token) {
-            if (preg_match('/\\b(to-?do|fix\\s*me|hack)\\b/i', $token[1])) {
+            if (preg_match('/\\b(to[ -]?do|fix\\s*me|hack)\\b/i', $token[1])) {
                 $this->printError('Todo comment left in code', $token[1], $token[2]);
             }
         }
